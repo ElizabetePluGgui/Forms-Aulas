513c601f382d0fe1fea78a926b899991f46fed44
Elizabete Fabri <67619417+ElizabetePluGgui@users.noreply.github.com>
2020-11-06 23:56:59 -0300

    ok

--- github_explorer/src/pages/Dashboard/index.tsx
+++ github_explorer/src/pages/Dashboard/index.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <h1>Dashboard</h1>;
+//   return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://avatars1.githubusercontent.com/u/67619417?s=460&u=bc9151a9a009071ebef8c49d9140247fda5340db&v=4" alt="Elizabete Fabri" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img src="https://avatars1.githubusercontent.com/u/67619417?s=460&u=bc9151a9a009071ebef8c49d9140247fda5340db&v=4" alt="Elizabete Fabri" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img src="https://avatars1.githubusercontent.com/u/67619417?s=460&u=bc9151a9a009071ebef8c49d9140247fda5340db&v=4" alt="Elizabete Fabri" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
