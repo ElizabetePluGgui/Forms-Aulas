0320b3ee407152bb3144be01e9cafdce2ee608d0
Elizabete Fabri <67619417+ElizabetePluGgui@users.noreply.github.com>
2020-11-07 18:36:04 -0300

    ok

--- github_explorer/src/styles/global.ts
+++ github_explorer/src/styles/global.ts
@@ -10,7 +10,7 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
   body {
-    background: #f0f0f5 url(${githubBackground}) no-repeat 70% top;
+    background: #F0F0F5 url(${githubBackground}) no-repeat 70% top;
     -webkit-font-smoothing: antialiased;
   }
   body, input, button {
